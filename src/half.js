01f0b9eee9aefa1d8cc665764b2326c7d899c2f3
menua agamirov <menuaone@gmail.com>
2025-06-23 23:41:20 +0300

    feat: add validator

--- src/half.js
+++ src/half.js
@@ -1,13 +1,111 @@
 // @ts-check
 
-/**
- * @param {Number} num
- */
-export default (num) => {
-  const result = num / 2
-  return result
+// /**
+//  * @param {Number} num
+//  */
+// export default (num) => {
+//     const result = num / 2;
+//     return result;
+// };
+
+export default class Validator {
+    // TODO: maybe change flags on null check
+    constructor(options = {}) {
+        this.type = 'no type';
+        this.minLengthNumber = null;
+        this.value = null;
+        this.options = {
+            isContain: false,
+            isRequired: false,
+            isCheckMinLength: false,
+            ...options,
+        };
+    }
+
+    string() {
+        this.type = 'string';
+    }
+
+    isValid(data) {
+        if (!data) {
+            return this.handleNoData();
+        }
+
+        const typeGuard = this.checkType(data);
+
+        if (!typeGuard) {
+            return console.log(`Type validation result: ${typeGuard}`);
+        }
+
+        // TODO: make validation pipeline
+        this.options.isCheckMinLength && this.validateMinLength(data);
+        this.options.isContain && this.validateContain(data);
+
+        return typeGuard;
+    }
+
+    handleNoData() {
+        if (!this.options.isRequired) {
+            console.log(
+                'Validation skipped: field is not required, but data is empty',
+                true
+            );
+            return true;
+        }
+
+        console.log('Validation failed: data is required', false);
+        return false;
+    }
+
+    // validators
+    checkType(data) {
+        const isValid = typeof data === this.type;
+        console.log(`Type validation result: ${isValid}`);
+        return isValid;
+    }
+
+    validateContain(data) {
+        console.log(`Contain validation result: ${data.includes(this.value)}`);
+        return data.includes(this.value);
+    }
+
+    validateMinLength(data) {
+        console.log(
+            `MinLength validation result: ${data.lenght > this.minLengthNumber}`
+        );
+        return data.lenght > this.minLengthNumber;
+    }
+
+    // builders
+    // TODO: maybe remove flags
+    contains(value) {
+        this.value = value;
+        this.options.isContain = true;
+        return this;
+    }
+
+    minLength(num) {
+        this.minLengthNumber = num;
+        this.options.isCheckMinLength = true;
+        return this;
+    }
+
+    required() {
+        this.options.isRequired = true;
+        return this;
+    }
+
+    getType() {
+        console.log(this.type);
+    }
+    getValue() {
+        console.log(this.value);
+    }
+    getMinLength() {
+        console.log(this.minLengthNumber);
+    }
 }
 
-export class Validtaor {
+const schema = new Validator();
 
-}
+schema.string();
